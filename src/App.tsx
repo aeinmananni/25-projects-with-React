ba73b862a4db34f9dab243e4dbf8c7f8a3bc986d
aein <mananniaein1234@gmail.com>
2024-11-27 09:35:10 +0330

    version level up

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 const App = () => {
   return (
-    <div>
-      <h1 className="bg-red-500">Wellcom To React</h1>
+    <div className="container mx-auto bg-sky-500 p-2 rounded-md">
+      <h1 className="text-white">
+        {"سلام من بزرگ ترین برنامه نویس جهان هستم"}
+      </h1>
     </div>
   );
 };
