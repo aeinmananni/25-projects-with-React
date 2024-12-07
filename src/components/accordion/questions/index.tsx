3c95ce1e3d14acd1915820bcd7ea4fd967b99501
aein <mananniaein1234@gmail.com>
2024-12-07 13:06:40 +0330

    add new Pckages

--- src/components/accordion/questions/index.tsx
+++ src/components/accordion/questions/index.tsx
@@ -1,18 +1,33 @@
+/* eslint-disable @typescript-eslint/no-unused-expressions */
+import { useState } from "react";
 import Button from "../../../custom/button";
 import { BiPlus } from "react-icons/bi";
 type QuestionProps = {
   className?: string;
+  title?: string;
+  exp?: string;
+  onClick?: () => void;
 };
 
-const Question = ({ className }: QuestionProps) => {
+const Question = ({ title, onClick, exp, className }: QuestionProps) => {
+  const [showInfo, setShowInfo] = useState<boolean>(false);
   return (
-    <div
-      className={`border flex items-center justify-between p-2 rounded-lg ${className}`}
-    >
-      <h1>TTTT</h1>
-      <Button className="bg-sky-800 text-white p-2 rounded-full">
-        {<BiPlus />}
-      </Button>
+    <div className="flex flex-col">
+      <div
+        className={`border flex items-center justify-between p-2 rounded-lg ${className}`}
+      >
+        <h1>{title}</h1>
+        <Button
+          onClick={() => {
+            onClick && onClick();
+            setShowInfo((c) => !c);
+          }}
+          className="bg-sky-800 text-white p-2 rounded-full"
+        >
+          {<BiPlus />}
+        </Button>
+      </div>
+      {showInfo && exp && <div className="w-full h-max p-2">{exp}</div>}
     </div>
   );
 };
