0a247ba271c2bc105cb6baffb230666c4ce6fd9f
aein <mananniaein1234@gmail.com>
2025-06-25 19:16:11 +0330

    add new chnage

--- src/components/slider/components/cart/cart.tsx
+++ src/components/slider/components/cart/cart.tsx
@@ -1,15 +1,17 @@
 import { Image } from "../../../../custom/img";
-import image1 from "../../images/image1.jpg";
+import { CartType } from "../../model";
 
-export default function Cart() {
+type CartProps = CartType;
+
+export default function Cart({ image, fullName, title, exp }: CartProps) {
   return (
-    <div className="w-full border h-full flex flex-col items-center gap-5 p-3 overflow-hidden">
-      <Image src={image1} className="w-48 h-32 rounded-lg" />
+    <div className="w-full border h-full flex flex-col items-center gap-5 p-3 shrink-0 bg-slate-200">
+      <Image src={image} className="w-48 h-32 rounded-lg" />
       <div className="flex flex-col items-center justify-center gap-1">
-        <span className="text-red-500 font-bold text-lg">{`ماریا کریمی`}</span>
-        <small>{`مهندس ساختمان`}</small>
+        <span className="text-red-500 font-bold text-xl">{fullName}</span>
+        <small className="text-slate-900">{title}</small>
       </div>
-      <p className="text-center">{`لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطر آنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می‌باشد.`}</p>
+      <p className="text-center text-slate-900">{exp}</p>
     </div>
   );
 }
--- src/components/slider/components/carts/carts.tsx
+++ src/components/slider/components/carts/carts.tsx
@@ -1,9 +1,17 @@
+import { CART_ITEMS } from "../../data";
 import { Cart } from "../cart";
-
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
+const arrow = "text-slate-800 cursor-pointer";
 export default function Carts() {
   return (
-    <div className="w-1/2  border rounded-lg overflow-hidden">
-      <Cart />
+    <div className="w-1/2  border rounded-lg overflow-hidden  flex relative">
+      {CART_ITEMS.map((it, index) => (
+        <Cart key={index} {...it} />
+      ))}
+      <div className="w-full absolute top-1/2  flex justify-between items-center">
+        <IoIosArrowForward className={arrow} size={30} />
+        <IoIosArrowBack className={arrow} size={30} />
+      </div>
     </div>
   );
 }
