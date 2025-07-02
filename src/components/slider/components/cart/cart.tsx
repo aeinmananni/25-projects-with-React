9fa965b50296e1b9f1e27cd8af3652db7d8bcc14
aein <mananniaein1234@gmail.com>
2025-07-02 21:49:00 +0330

    asdasd

--- src/components/slider/components/cart/cart.tsx
+++ src/components/slider/components/cart/cart.tsx
@@ -1,11 +1,24 @@
 import { Image } from "../../../../custom/img";
 import { CartType } from "../../model";
 
-type CartProps = CartType;
+type CartProps = CartType & {
+  className?: string;
+  style?: React.CSSProperties;
+};
 
-export default function Cart({ image, fullName, title, exp }: CartProps) {
+export default function Cart({
+  image,
+  fullName,
+  title,
+  exp,
+  className,
+  style,
+}: CartProps) {
   return (
-    <div className="w-full border h-full flex flex-col items-center gap-5 p-3 shrink-0 bg-slate-200">
+    <div
+      style={style}
+      className={`w-full border transition-all duration-500 h-full flex flex-col items-center gap-5 p-3 shrink-0 bg-slate-200 ${className}`}
+    >
       <Image src={image} className="w-48 h-32 rounded-lg" />
       <div className="flex flex-col items-center justify-center gap-1">
         <span className="text-red-500 font-bold text-xl">{fullName}</span>
